1d212d9f4d53d0f74f308cf756aad2a32bf53eed
Atharva Patil <apatil@Atharva-Ps-MacBook-Pro.local>
2022-12-01 12:53:02 -0800

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
+use std::error::Error;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
-use std::env;
-use std::fs;
 use std::collections::BinaryHeap;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // Day 1 
-    println!("Hello, world!");
     let f = File::open("./day1.txt").expect("Unable to open file");
     let f = BufReader::new(f);
     let mut heap: BinaryHeap<u32> = BinaryHeap::new();
-    let mut max_sum: u32 = 0;
     let mut curr_sum:u32 = 0;
     for line in f.lines() {
         let line = line.expect("Unable to read line");
         if line.eq("") {
             heap.push(curr_sum);
-            max_sum = std::cmp::max(max_sum,curr_sum);
             curr_sum = 0;
         } else {
-            let curr_val: u32 = line.parse().unwrap();
+            let curr_val: u32 = line.parse()?;
             curr_sum += curr_val;
         }
     }
     //part 1
-    println!("top value {:?}", max_sum);
+    println!("top value {:?}", heap.peek());
 
     //part 2
     let mut top_3_sum:u32 = 0;
@@ -32,4 +28,6 @@ fn main() {
         top_3_sum += heap.pop().unwrap();
     }
     println!("top 3 sum : {:?}", top_3_sum);
+    
+    Ok(())
 }
